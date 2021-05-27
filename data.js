9bd94b6d12a510c4164d47c2476855546a7d34c0
claudia0703 <84883086+claudia0703@users.noreply.github.com>
2021-05-27 01:13:52 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,84 @@
+const data={
+    categories: [
+        {
+            name:"Beverages",
+            image:"/images/bever.png",
+        },
+        {
+            name: "Burgers",
+            image:"/images/burger.png",
+        },
+        {
+            name:"Dessert",
+            image:"/images/dessert.png",
+        },
+    ],
+
+    products: [
+        {
+        category: "Beverages",
+        name: "Coca-Cola",
+        calorie: 120,
+        price: 1,
+        image: "/images/small_coca.jpg",
+    },
+    {
+        category: "Beverages",
+        name: "Sprite",
+        calorie: 155,
+        price: 1,
+        image: "/images/sprite.png",
+    },
+    {
+        category: "Beverages",
+        name: "Mountain Dew",
+        calorie: 170,
+        price: 1,
+        image: "/images/mountaindew.jpeg",
+    },
+    {
+        category: "Burgers",
+        name: "Double Cheeseburger",
+        calorie: 270,
+        price: 3.50,
+        image: "/images/double_cheeseb.png",
+    },
+    {
+        category: "Burgers",
+        name: "BBQ Bacon Burger",
+        calorie: 350,
+        price: 4.50,
+        image: "/images/bbq_bacon_burger.jpg",
+    },
+    {
+        category: "Burgers",
+        name: "Crispy Chicken",
+        calorie: 245,
+        price: 3,
+        image: "/images/crispy_chicken.jpg",
+    },
+    {
+        category: "Dessert",
+        name: "Peach Mousse",
+        calorie: 90,
+        price: 0.90,
+        image: "/images/peach_mousse.jpg",
+    },
+    {
+        category: "Dessert",
+        name: "Strawberry Cheesecake",
+        calorie: 300,
+        price: 1.50,
+        image: "/images/strawberry_cake.png",
+    },
+    {
+        category: "Dessert",
+        name: "Vegan Apple Pie",
+        calorie: 175,
+        price: 1.50,
+        image: "/images/vegan_applepie.jpg",
+    },
+  ]
+};
+
+module.exports = data;
